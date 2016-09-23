4da00eabe653ed64a888e1f9e3e4be98ca30218b
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-09-23 09:35:31 -0700

    Add tests to ClientTest.java and methods to Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -7,131 +7,131 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class Animal{
+public class Client{
   private String name;
-  private Date admittance;
-  private String gender;
-  private String type;
-  private String breed;
+  private String stylist;
+  private Date appointment;
+  private String haircut;
+  private String color;
   private int id;
-  private int owner_id;
+  private int stylist_id;
 
-  public Animal(String name, String admittance, String gender, String type, String breed){
+  public Client(String name, String stylist, String appointment, String haircut, String color){
     this.name = name;
-    this.gender = gender;
-    this.type = type;
-    this.breed= breed;
+    this.stylist = stylist;
+    this.haircut = haircut;
+    this.color= color;
     DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
     try {
-    this.admittance = formatter.parse(admittance);
+    this.appointment = formatter.parse(appointment);
   } catch (Exception e){
-      System.out.print("whoops");
+      System.out.print("Please add a valid appointment date.");
     }
-    this.owner_id = -1;
+    this.stylist_id = 0;
   }
 
   public String getName(){
     return name;
   }
 
-  public Date getAdmittance(){
-    return admittance;
+  public String getStylist(){
+    return stylist;
   }
 
-  public String getGender(){
-    return gender;
+  public Date getAppointment(){
+    return appointment;
   }
 
-  public String getType(){
-    return type;
+  public String getHaircut(){
+    return haircut;
   }
 
-  public String getBreed(){
-    return breed;
+  public String getColor(){
+    return color;
   }
 
   public int getId(){
     return id;
   }
 
-  public int getOwnerId(){
-    return owner_id;
+  public int getStylistId(){
+    return stylist_id;
   }
 
-  public void setOwnerId(int owner_id){
-    this.owner_id = owner_id;
+  public void setStylistId(int stylist_id){
+    this.stylist_id = stylist_id;
     try(Connection con = DB.sql2o.open()) {
-      String sql = "UPDATE animals SET owner_id = :owner_id WHERE id = :id";
+      String sql = "UPDATE clients SET stylist_id = :stylists_id WHERE id = :id";
       con.createQuery(sql)
-        .addParameter("owner_id", owner_id)
+        .addParameter("stylist_id", stylist_id)
         .addParameter("id", id)
         .executeUpdate();
     }
   }
-
-  @Override
-  public boolean equals(Object otherAnimal) {
-    if(!(otherAnimal instanceof Animal)){
-      return false;
-    } else {
-      Animal newAnimal = (Animal) otherAnimal;
-      return this.name.equals(newAnimal.name) && this.admittance.equals(newAnimal.admittance) &&
-        this.gender.equals(newAnimal.gender) && this.type.equals(newAnimal.type) && this.getBreed().equals(newAnimal.getBreed()) && this.id == newAnimal.id;
-    }
-  }
-
-  public void save(){
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "INSERT INTO animals(name, admittance, gender, type, breed, owner_id) VALUES (:name, :admittance, :gender, :type, :breed, :owner_id)";
-      this.id = (int) con.createQuery(sql, true)
-        .addParameter("name", this.name)
-        .addParameter("admittance", this.admittance)
-        .addParameter("gender", this.gender)
-        .addParameter("type", this.type)
-        .addParameter("breed", this.breed)
-        .addParameter("owner_id", this.owner_id)
-        .executeUpdate()
-        .getKey();
-    }
-  }
-
-  public static List<Animal> all(){
-    String sql="SELECT * FROM animals";
-    try(Connection con = DB.sql2o.open()) {
-      return con.createQuery(sql).executeAndFetch(Animal.class);
-    }
-  }
-
-  public static List<Animal> sort(String sortby){
-    String sql="SELECT * FROM animals ORDER BY :sortby";
-    try(Connection con = DB.sql2o.open()) {
-      return con.createQuery(sql).addParameter("sortby", sortby).executeAndFetch(Animal.class);
-    }
-  }
-
-  public static Set<String> getTypes(){
-    Set<String> typeSet = new TreeSet<String>();
-    for(Animal animal: Animal.all()){
-      typeSet.add(animal.type);
-    }
-    return typeSet;
-  }
-
-  public static Set<String> getBreeds(){
-    Set<String> breedSet = new TreeSet<String>();
-    for(Animal animal: Animal.all()){
-      breedSet.add(animal.breed);
-    }
-    return breedSet;
-  }
-
-  public static Animal find(int id){
-    try(Connection con = DB.sql2o.open()){
-      String sql = "SELECT * FROM animals WHERE id=:id";
-      Animal animal = con.createQuery(sql)
-      .addParameter("id", id)
-      .executeAndFetchFirst(Animal.class);
-      return animal;
-    }
-  }
+  //
+  // @Override
+  // public boolean equals(Object otherAnimal) {
+  //   if(!(otherAnimal instanceof Animal)){
+  //     return false;
+  //   } else {
+  //     Animal newAnimal = (Animal) otherAnimal;
+  //     return this.name.equals(newAnimal.name) && this.admittance.equals(newAnimal.admittance) &&
+  //       this.gender.equals(newAnimal.gender) && this.type.equals(newAnimal.type) && this.getBreed().equals(newAnimal.getBreed()) && this.id == newAnimal.id;
+  //   }
+  // }
+  //
+  // public void save(){
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "INSERT INTO animals(name, admittance, gender, type, breed, owner_id) VALUES (:name, :admittance, :gender, :type, :breed, :owner_id)";
+  //     this.id = (int) con.createQuery(sql, true)
+  //       .addParameter("name", this.name)
+  //       .addParameter("admittance", this.admittance)
+  //       .addParameter("gender", this.gender)
+  //       .addParameter("type", this.type)
+  //       .addParameter("breed", this.breed)
+  //       .addParameter("owner_id", this.owner_id)
+  //       .executeUpdate()
+  //       .getKey();
+  //   }
+  // }
+  //
+  // public static List<Animal> all(){
+  //   String sql="SELECT * FROM animals";
+  //   try(Connection con = DB.sql2o.open()) {
+  //     return con.createQuery(sql).executeAndFetch(Animal.class);
+  //   }
+  // }
+  //
+  // public static List<Animal> sort(String sortby){
+  //   String sql="SELECT * FROM animals ORDER BY :sortby";
+  //   try(Connection con = DB.sql2o.open()) {
+  //     return con.createQuery(sql).addParameter("sortby", sortby).executeAndFetch(Animal.class);
+  //   }
+  // }
+  //
+  // public static Set<String> getTypes(){
+  //   Set<String> typeSet = new TreeSet<String>();
+  //   for(Animal animal: Animal.all()){
+  //     typeSet.add(animal.type);
+  //   }
+  //   return typeSet;
+  // }
+  //
+  // public static Set<String> getBreeds(){
+  //   Set<String> breedSet = new TreeSet<String>();
+  //   for(Animal animal: Animal.all()){
+  //     breedSet.add(animal.breed);
+  //   }
+  //   return breedSet;
+  // }
+  //
+  // public static Animal find(int id){
+  //   try(Connection con = DB.sql2o.open()){
+  //     String sql = "SELECT * FROM animals WHERE id=:id";
+  //     Animal animal = con.createQuery(sql)
+  //     .addParameter("id", id)
+  //     .executeAndFetchFirst(Animal.class);
+  //     return animal;
+  //   }
+  // }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -8,147 +8,65 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class AnimalTest{
-  Animal myAnimal;
+public class ClientTest{
+  Client myClient;
   @Before
   public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/shelter_test", null, null);
-    myAnimal = new Animal("Steve", "2016-06-10", "M", "cat", "asshole");
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+    myClient = new Client("Vanessa","Ilena", "2016-09-29", "Y", "Y");
   }
 
   @After
   public void tearDown() {
     try(Connection con = DB.sql2o.open()) {
-      String sql_animals = "DELETE FROM animals *;";
-      String sql_owners = "DELETE FROM owners *;";
-      con.createQuery(sql_owners).executeUpdate();
-      con.createQuery(sql_animals).executeUpdate();
+      String sql_clients = "DELETE FROM clients *;";
+      String sql_stylists= "DELETE FROM stylists *;";
+      con.createQuery(sql_clients).executeUpdate();
+      con.createQuery(sql_stylists).executeUpdate();
     }
   }
 
   @Test
-  public void animal_instantiatesCorrectly() {
-    assertTrue(myAnimal instanceof Animal);
+  public void client_instantiatesCorrectly() {
+   assertTrue(myClient instanceof Client);
+ }
+
+  @Test
+  public void getName_ClientInstantiatesWithName_Vanessa() {
+    Client myClient = new Client("Vanessa", "Ilena", "2016-09-29", "Y", "Y");
+    assertEquals("Vanessa", myClient.getName());
+ }
+
+ @Test
+ public void getStylist_returnsStylist_String(){
+   assertEquals("Ilena", myClient.getStylist());
+ }
+
+  @Test
+  public void getAppointment_returnsAppointment_Date(){
+     DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+     Date date=null;
+     try{
+       date = formatter.parse("2016-09-29");
+     } catch(Exception e){
+       System.out.print("Please choose an appointment date");
+     }
+     assertEquals(date, myClient.getAppointment());
+   }
+
+  @Test
+  public void getHaircut_returnsHaircut_String(){
+    assertEquals("Y", myClient.getHaircut());
   }
 
   @Test
-  public void getAdmittance_returnsAdmittance_Date(){
-    DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-    Date date=null;
-    try{
-      date = formatter.parse("2016-06-10");
-    } catch(Exception e){
-      System.out.print("aieee");
-    }
-    assertEquals(date, myAnimal.getAdmittance());
+  public void getColor_returnsColor_String(){
+    assertEquals("Y", myClient.getColor());
   }
 
   @Test
-  public void getGender_returnsGender_String(){
-    assertEquals("M", myAnimal.getGender());
-  }
+   public void getStylistId_instantiatesToZero_Zero(){
+     assertEquals(0, myClient.getStylistId());
+   }
 
-  @Test
-  public void getType_returnsType_String(){
-    assertEquals("cat", myAnimal.getType());
-  }
-
-  @Test
-  public void getBreed_returnsBreed_String(){
-    assertEquals("asshole", myAnimal.getBreed());
-  }
-
-  @Test
-  public void getOwnerId_instantiatesToNeg1_Neg1(){
-    assertEquals(-1, myAnimal.getOwnerId());
-  }
-
-  @Test
-  public void getOwnerId_changesWhenSet_1(){
-    myAnimal.setOwnerId(1);
-    assertEquals(1, myAnimal.getOwnerId());
-  }
-
-  @Test
-  public void equals_returnsTrueIfFieldsAreTheSame(){
-    Animal myOtherAnimal = new Animal("Steve", "2016-06-10", "M", "cat", "asshole");
-    assertTrue(myAnimal.equals(myOtherAnimal));
-  }
-
-  @Test
-  public void save_savesAnimalToDatabase(){
-    myAnimal.save();
-    String sql = "SELECT * FROM animals WHERE name = 'Steve'";
-    Animal fetchedAnimal;
-    try(Connection con = DB.sql2o.open()) {
-      fetchedAnimal = con.createQuery(sql).executeAndFetchFirst(Animal.class);
-    }
-    assertTrue(myAnimal.equals(fetchedAnimal));
-  }
-
-  @Test
-  public void all_returnsAllInstancesOfAnimals_true(){
-    myAnimal.save();
-    Animal myOtherAnimal = new Animal("Bobette", "2016-06-10", "F", "dog", "poodle");
-    myOtherAnimal.save();
-    assertTrue(Animal.all().contains(myAnimal));
-    assertTrue(Animal.all().contains(myOtherAnimal));
-  }
-
-  @Test
-  public void sort_sortsByGivenColumns_true(){
-    myAnimal.save();
-    Animal myOtherAnimal = new Animal("Bobette", "2016-06-10", "F", "dog", "poodle");
-    myOtherAnimal.save();
-    assertEquals(myAnimal, Animal.all().get(0));
-    assertEquals(myOtherAnimal, Animal.sort("name").get(0));
-  }
-
-  @Test
-  public void getId_animalsInstantiateWithAnID(){
-    myAnimal.save();
-    assertTrue(myAnimal.getId() > 0);
-  }
-
-  @Test
-  public void find_returnsAnimalWithSameId_otherAnimal(){
-    myAnimal.save();
-    Animal myOtherAnimal = new Animal("Bobette", "2016-06-10", "F", "dog", "poodle");
-    myOtherAnimal.save();
-    assertEquals(Animal.find(myOtherAnimal.getId()), myOtherAnimal);
-  }
-
-  @Test
-  public void getTypes_returnsAnimalTypes_True(){
-    myAnimal.save();
-    Animal myOtherAnimal = new Animal("Bobette", "2016-06-10", "F", "dog", "poodle");
-    myOtherAnimal.save();
-    assertTrue(Animal.getTypes().contains("cat"));
-    assertTrue(Animal.getTypes().contains("dog"));
-  }
-
-  @Test
-  public void getTypes_doesNotSaveDuplicates_True(){
-    myAnimal.save();
-    Animal myOtherAnimal = new Animal("Bobette", "2016-06-10", "F", "cat", "poodle");
-    myOtherAnimal.save();
-    assertEquals(1, Animal.getTypes().size());
-  }
-
-  @Test
-  public void getBreeds_returnsAnimalBreeds_True(){
-    myAnimal.save();
-    Animal myOtherAnimal = new Animal("Bobette", "2016-06-10", "F", "dog", "poodle");
-    myOtherAnimal.save();
-    assertTrue(Animal.getBreeds().contains("asshole"));
-    assertTrue(Animal.getBreeds().contains("poodle"));
-  }
-
-  @Test
-  public void getBreeds_doesNotSaveDuplicates_True(){
-    myAnimal.save();
-    Animal myOtherAnimal = new Animal("Bobette", "2016-06-10", "F", "cat", "asshole");
-    myOtherAnimal.save();
-    assertEquals(1, Animal.getBreeds().size());
-  }
 }
